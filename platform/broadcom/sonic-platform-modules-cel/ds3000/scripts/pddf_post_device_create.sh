559a919bc923057d09054ff0847cef974f2461a3
Eric Zhu <erzhu@celestica.com>
2023-08-14 01:22:18 -0500

    DS3000 platform fix for bmc and nonbmc
    1. default enable rs fec
    2. add more firmware components
    3. Add fpga extend driver

--- platform/broadcom/sonic-platform-modules-cel/ds3000/scripts/pddf_post_device_create.sh
+++ platform/broadcom/sonic-platform-modules-cel/ds3000/scripts/pddf_post_device_create.sh
@@ -3,6 +3,9 @@
 declare -r CPLD_SETREG="/sys/bus/platform/devices/baseboard/setreg"
 declare -r CPLD_GETREG="/sys/bus/platform/devices/baseboard/getreg"
 
+# Load fpga extend driver after fpga device created
+modprobe pddf_custom_fpga_extend
+
 # Set SYS_LED to Green, assuming everything came up fine.
 echo "0xa162 0xdc" > ${CPLD_SETREG}
 
