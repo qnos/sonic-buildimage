314b9e0aaa549513dda9d05addb9cdbb40922436
Eric Zhu <erzhu@celestica.com>
2023-11-02 20:06:08 -0500

    DS3000 thermal threshold and sensors fix
    1. fix thermal threshold issue for BMC and NonBMC
    2. Add sensors configuration

--- platform/broadcom/sonic-platform-modules-cel/ds3000/pddf/sonic_platform/thermal.py
+++ platform/broadcom/sonic-platform-modules-cel/ds3000/pddf/sonic_platform/thermal.py
@@ -12,3 +12,16 @@ class Thermal(PddfThermal):
         PddfThermal.__init__(self, index, pddf_data, pddf_plugin_data, is_psu_thermal, psu_index)
 
     # Provide the functions/variables below for which implementation is to be overwritten
+
+    def get_high_threshold(self):
+        if self.is_psu_thermal:
+            device = "PSU{}".format(self.thermals_psu_index)
+            output = self.pddf_obj.get_attr_name_output(device, "psu_temp1_high_threshold")
+            if not output:
+                return None
+
+            temp1 = output['status']
+            # temperature returned is in milli celcius
+            return float(temp1)/1000
+        else:
+            return super().get_high_threshold()
