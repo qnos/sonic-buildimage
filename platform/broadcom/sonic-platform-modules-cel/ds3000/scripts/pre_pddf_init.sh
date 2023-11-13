727a1580e88737da7e6d79e2e7d32f4df705a1d7
Eric Zhu <erzhu@celestica.com>
2023-11-13 07:06:35 -0600

    DS3000 fix show platform firmware updates issue

--- platform/broadcom/sonic-platform-modules-cel/ds3000/scripts/pre_pddf_init.sh
+++ platform/broadcom/sonic-platform-modules-cel/ds3000/scripts/pre_pddf_init.sh
@@ -15,9 +15,16 @@ PDDF_JSON="pddf-device.json"
 PDDF_JSON_BMC="pddf-device-bmc.json"
 PDDF_JSON_NONBMC="pddf-device-nonbmc.json"
 PDDF_JSON_PATH="/usr/share/sonic/device/${PLATFORM}/pddf"
+PLATFORM_COMPONENTS_FILE="/usr/share/sonic/device/${PLATFORM}/platform_components.json"
 if [ ${BMC_PRESENCE} == "present" ]; then
     cp ${PDDF_JSON_PATH}/${PDDF_JSON_BMC} ${PDDF_JSON_PATH}/${PDDF_JSON}
+    # Add BMC component if BMC exists
+    if ! grep -q "BMC" ${PLATFORM_COMPONENTS_FILE}; then
+        sed -i '6i \                "BMC": {},' ${PLATFORM_COMPONENTS_FILE}
+    fi
 else
     # BMC Card absent
     cp ${PDDF_JSON_PATH}/${PDDF_JSON_NONBMC} ${PDDF_JSON_PATH}/${PDDF_JSON}
+    # Remove BMC component for NonBMC
+    sed -i '/"BMC"/d' ${PLATFORM_COMPONENTS_FILE}
 fi
